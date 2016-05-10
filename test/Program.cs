2101d552573661ffb93f66db51f17cf114207df3
mirsakiyev <mirsakiyev@yahoo.com>
2016-05-10 14:14:14 -0400

    Added HelloWorld Console to method

--- test/Program.cs
+++ test/Program.cs
@@ -10,6 +10,18 @@ namespace test
     {
         static void Main(string[] args)
         {
+            // call the Hello World Console method
+            HelloWorldToConsole();
+        }
+
+        // @method HelloWorldToConsole
+        // @description This is my Hello World Method
+        // @return {string} HelloWorldString
+        public static string HelloWorldToConsole()
+        {
+            string HelloWorldString = "Hello, World!";
+            Console.WriteLine(HelloWorldString);
+            return HelloWorldString;
         }
     }
 }
