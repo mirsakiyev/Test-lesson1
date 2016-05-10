c12bd0219ca0eea7f47e0dd8cac6b7333202f71a
mirsakiyev <mirsakiyev@yahoo.com>
2016-05-10 14:25:14 -0400

    TestExplorer

---
+++ HelloWorldTestProject/HelloWorldUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTestProject
+{
+    [TestClass]
+    public class HelloWorldUnitTest
+    {
+        [TestMethod]
+        public void HelloWorldToConsoleTestMethod()
+        {
+            // Arrange
+            string expectedResult = "Hello World";
+            // Act
+            string result = test.Program.HelloWorldToConsole();
+            // Asert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
